2551a6c06abf4310ab0a50acc041edd927581161
Adam Trepka <kleszczowiec@gmail.com>
2019-01-24 22:12:41 +0100

    Pierwsza wersja programu

---
+++ Skaner/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Skaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.InputList = new System.Windows.Forms.ListView();
+            this.ordinalNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.serialNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.inventoryNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.CheckedList = new System.Windows.Forms.ListView();
+            this.ordinalNumberCheck = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.serialNumberCheck = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.inventoryNumberCheck = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.inputCounter = new System.Windows.Forms.Label();
+            this.checkCounter = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.InputList);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(380, 506);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lista";
+            // 
+            // InputList
+            // 
+            this.InputList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ordinalNumber,
+            this.serialNumber,
+            this.inventoryNumber});
+            this.InputList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.InputList.Location = new System.Drawing.Point(3, 16);
+            this.InputList.Name = "InputList";
+            this.InputList.Size = new System.Drawing.Size(374, 487);
+            this.InputList.TabIndex = 0;
+            this.InputList.UseCompatibleStateImageBehavior = false;
+            this.InputList.View = System.Windows.Forms.View.Details;
+            // 
+            // ordinalNumber
+            // 
+            this.ordinalNumber.Text = "Lp.";
+            this.ordinalNumber.Width = 50;
+            // 
+            // serialNumber
+            // 
+            this.serialNumber.Text = "Numer seryjny";
+            this.serialNumber.Width = 150;
+            // 
+            // inventoryNumber
+            // 
+            this.inventoryNumber.Text = "Numer inwentarzowy";
+            this.inventoryNumber.Width = 150;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.Controls.Add(this.CheckedList);
+            this.groupBox2.Location = new System.Drawing.Point(389, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(381, 506);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Odczytane";
+            // 
+            // CheckedList
+            // 
+            this.CheckedList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ordinalNumberCheck,
+            this.serialNumberCheck,
+            this.inventoryNumberCheck});
+            this.CheckedList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CheckedList.Location = new System.Drawing.Point(3, 16);
+            this.CheckedList.Name = "CheckedList";
+            this.CheckedList.Size = new System.Drawing.Size(375, 487);
+            this.CheckedList.TabIndex = 0;
+            this.CheckedList.UseCompatibleStateImageBehavior = false;
+            this.CheckedList.View = System.Windows.Forms.View.Details;
+            // 
+            // ordinalNumberCheck
+            // 
+            this.ordinalNumberCheck.Text = "Lp.";
+            // 
+            // serialNumberCheck
+            // 
+            this.serialNumberCheck.Text = "Numer seryjny";
+            this.serialNumberCheck.Width = 150;
+            // 
+            // inventoryNumberCheck
+            // 
+            this.inventoryNumberCheck.Text = "Numer inwentarzowy";
+            this.inventoryNumberCheck.Width = 150;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(3, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(514, 40);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.AutoSize = true;
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Location = new System.Drawing.Point(141, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(520, 65);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Wprowadź numer seryjny";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox4.AutoSize = true;
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Location = new System.Drawing.Point(667, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(121, 68);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Liczba porządkowa";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "####";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 103);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(773, 512);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 65);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Wczytaj plik z numerami";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Plik danych| *.csv";
+            this.openFileDialog1.Title = "Wybierz plik";
+            // 
+            // inputCounter
+            // 
+            this.inputCounter.AutoSize = true;
+            this.inputCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.inputCounter.Location = new System.Drawing.Point(17, 80);
+            this.inputCounter.Name = "inputCounter";
+            this.inputCounter.Size = new System.Drawing.Size(128, 20);
+            this.inputCounter.TabIndex = 9;
+            this.inputCounter.Text = "Wprowadzono: 0";
+            // 
+            // checkCounter
+            // 
+            this.checkCounter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkCounter.AutoSize = true;
+            this.checkCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkCounter.Location = new System.Drawing.Point(686, 80);
+            this.checkCounter.Name = "checkCounter";
+            this.checkCounter.Size = new System.Drawing.Size(102, 20);
+            this.checkCounter.TabIndex = 10;
+            this.checkCounter.Text = "Odczytano: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 627);
+            this.Controls.Add(this.checkCounter);
+            this.Controls.Add(this.inputCounter);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "Skaner";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView InputList;
+        private System.Windows.Forms.ListView CheckedList;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColumnHeader ordinalNumber;
+        private System.Windows.Forms.ColumnHeader serialNumber;
+        private System.Windows.Forms.ColumnHeader inventoryNumber;
+        private System.Windows.Forms.ColumnHeader ordinalNumberCheck;
+        private System.Windows.Forms.ColumnHeader serialNumberCheck;
+        private System.Windows.Forms.ColumnHeader inventoryNumberCheck;
+        private System.Windows.Forms.Label inputCounter;
+        private System.Windows.Forms.Label checkCounter;
+    }
+}
+
---
+++ Skaner/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Skaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var fileStream = openFileDialog1.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var row = line.Split(';');
+                        InputList.Items.Add(new ListViewItem(row));
+                    }
+                }
+                inputCounter.Text = "Wprowadzono: " + InputList.Items.Count;
+            }
+            
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if (InputList.Items.Count == 0)
+                {
+                    MessageBox.Show("Lista przedmiotów jest pusta. Proszę wczytać plik z danymi.");
+                    label1.Text = "####";
+                }
+                else
+                {
+                    var foundItem = InputList.Items.Cast<ListViewItem>().Where(x => x.SubItems[1].Text == textBox1.Text).FirstOrDefault();
+                    if(foundItem != null)
+                    {
+                        label1.Text = foundItem.SubItems[0].Text;
+                        CheckedList.Items.Add((ListViewItem)foundItem.Clone());
+                        InputList.Items.Remove(foundItem);
+                        textBox1.Text = "";
+                        this.ActiveControl = textBox1;
+                        checkCounter.Text = "Odczytano: " + CheckedList.Items.Count;
+                    }
+                } 
+            }
+        }
+    }
+}
